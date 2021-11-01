20cb9b9bdfc4016b7a8db8d1cbf45e762b3c52c0
Tomerporian <tomerpor@gmail.com>
2021-11-01 19:43:13 +0200

    Adding number of epochs to params

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import utils as ut
 import autoencoder as ae
 import torch
 
-params = {'bsize': 3}
+params = {'bsize': 128,
+          'n_epochs': 5}
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 net=ae.Net()
@@ -11,7 +12,7 @@ optimizer=ae.optimizer
 criterion=ae.criterion
 dataloader=ut.get_pics(params)
 
-for epoch in range(2):
+for epoch in range(params['n_epochs']):
     for i,data in enumerate(dataloader,0):
         inputs, labels = data[0].to(device), data[1].to(device)
         optimizer.zero_grad()
